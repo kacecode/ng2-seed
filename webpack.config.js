c92d1718d60e086bcd2a616e9a81f53b8f5acc17
Kacey Cole <kcole@izeni.com>
2015-08-23 16:32:43 -0600

    Add es6 compile and move index.html to build dir

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,18 @@
 module.exports = {
     entry: './src/index.js',
     output: {
+        path: './build',
         filename: 'bin.js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader' }
+        ]
+    },
+    resolve: {
+        extensions: ['', '.js', '.json', '.coffee']
+    },
+    devServer: {
+        contentBase: './build'
     }
-}
+};
