8efa66eab1dcebbd7ca2f742ceda11c75ab35c2d
Kacey Cole <kcole@izeni.com>
2015-09-11 15:29:57 -0600

    A little more complicated logic in shopping list form

--- src/shopping-list.ts
+++ src/shopping-list.ts
@@ -1,6 +1,20 @@
 /// <reference path="./typings/angular2/angular2.d.ts"/>
 import {Component, View, NgFor} from 'angular2/angular2';
 
+class ShoppingItem {
+    value: string;
+    text: string;
+
+    constructor (text:string) {
+        this.text = text;
+        this.value = text.toLowerCase();
+    }
+
+    compare (thing:ShoppingItem) {
+        return this.value === thing.value;
+    }
+}
+
 @Component({
     selector: "shopping-list"
 })
@@ -10,21 +24,26 @@ import {Component, View, NgFor} from 'angular2/angular2';
     <p>{{ thing.value }}</p>
     <button (click)="addThing(thing.value)">Add It!</button>
     <ul>
-        <li *ng-for="#item of items">{{ item }}</li>
+        <li *ng-for="#item of items">{{ item.text }}</li>
     </ul>
     `,
     directives: [NgFor]
 })
 export class ShoppingList {
-    items:Array<string>;
+    items:Array<ShoppingItem>;
 
     constructor() {
-        this.items = ['Steak', 'Potatoes']
+        this.items = [
+            new ShoppingItem("Steak"),
+            new ShoppingItem("Potatoes")
+        ]
     }
 
     addThing(thing:string) {
-        if (this.items.indexOf(thing) === -1) {
-            this.items.push(thing);
+        let data = new ShoppingItem(thing);
+
+        if (!this.items.some((item) => data.compare(item))) {
+            this.items.push(data);
         }
     }
 }
